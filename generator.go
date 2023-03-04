7b8b4cd55e15fde6bd6d2b558b2d690c21af9426
iyear <ljyngup@gmail.com>
2023-03-04 18:30:19 +0800

    refactor(generator): make write public

--- generator.go
+++ generator.go
@@ -43,39 +43,42 @@ func NewGenerator(f string) (*Generator, error) {
 	return g, nil
 }
 
-func (g *Generator) Generate(w io.Writer) error {
+func (g *Generator) Generate() ([]cueast.Decl, error) {
 	var decls []cueast.Decl
-
 	for _, syntax := range g.pkg.Syntax {
 		for _, decl := range syntax.Decls {
 			if d, ok := decl.(*goast.GenDecl); ok {
 				t, err := g.convertDecls(d)
 				if err != nil {
-					return err
+					return nil, err
 				}
 				decls = append(decls, t...)
 			}
 		}
 	}
 
+	return decls, nil
+}
+
+func (g *Generator) Write(w io.Writer, decls []cueast.Decl) error {
 	pkg := &cueast.Package{Name: ident(g.pkg.Name, false)}
 
 	f := &cueast.File{Decls: []cueast.Decl{pkg}}
 	f.Decls = append(f.Decls, decls...)
 
-	return g.write(w, f)
-}
-
-func (g *Generator) write(w io.Writer, f *cueast.File) error {
 	if err := astutil.Sanitize(f); err != nil {
 		return err
 	}
 
-	b, err := cueformat.Node(f, cueformat.Simplify())
+	b, err := cueformat.Node(f, cueformat.Simplify(), cueformat.TabIndent(true))
 	if err != nil {
 		return err
 	}
 
 	_, err = w.Write(b)
 	return err
+}
+
+func (g *Generator) Package() *packages.Package {
+	return g.pkg
 }
